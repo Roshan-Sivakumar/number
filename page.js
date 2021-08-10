b7a52a85c9babbe6fdd8bac10421050bba6bca1f
Roshan <65855350+Roshan-Sivakumar@users.noreply.github.com>
2021-08-10 09:38:43 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,4 @@
+var name1 = localStorage.getItem("name1");
+var name2 = localStorage.getItem("name2");
+document.getElementById("player1name").innerHTML=name1 + ": ";
+document.getElementById("player2name").innerHTML=name2 + ": ";
